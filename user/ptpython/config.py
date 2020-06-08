87028258b0cad82e1c5414dd38c2fc16d7a5a83b
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-06-08 18:07:10 +0100

    updated user files

--- user/ptpython/config.py
+++ user/ptpython/config.py
@@ -23,59 +23,41 @@ def configure(repl):
     """
     # Show function signature (bool).
     repl.show_signature = True
-
     # Show docstring (bool).
     repl.show_docstring = True
-
-    # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
-    # inserts a newline instead of executing the code.
+    # Show the "[Meta+Enter] Execute" message when pressing [Enter]
     repl.show_meta_enter_message = True
-
     # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
-    repl.completion_visualisation = CompletionVisualisation.POP_UP
-
+#     repl.completion_visualisation = CompletionVisualisation.POP_UP
+    repl.completion_visualisation = CompletionVisualisation.MULTI_COLUMN
     # When CompletionVisualisation.POP_UP has been chosen, use this
     # scroll_offset in the completion menu.
     repl.completion_menu_scroll_offset = 0
-
     # Show line numbers (when the input contains multiple lines.)
     repl.show_line_numbers = True
-
     # Show status bar.
     repl.show_status_bar = False
-
     # When the sidebar is visible, also show the help text.
     repl.show_sidebar_help = True
-
     # Swap light/dark colors on or off
     repl.swap_light_and_dark = False
-
     # Highlight matching parethesis.
     repl.highlight_matching_parenthesis = True
-
     # Line wrapping. (Instead of horizontal scrolling.)
     repl.wrap_lines = True
-
     # Mouse support.
     repl.enable_mouse_support = False
-
-    # Complete while typing. (Don't require tab before the
-    # completion menu is shown.)
+    # Complete while typing.
     repl.complete_while_typing = True
-
     # Fuzzy and dictionary completion.
     repl.enable_fuzzy_completion = False
     repl.enable_dictionary_completion = True
-
     # Vi mode.
     repl.vi_mode = True
-
     # Paste mode. (When True, don't insert whitespace after new line.)
     repl.paste_mode = False
-
-    # Use the classic prompt. (Display '>>>' instead of 'In [1]'.)
+    # Prompt: classic '>>>' or ipython 'In [1]'
     repl.prompt_style = "ipython"  # 'classic' or 'ipython'
-
     # Don't insert a blank line after the output.
     repl.insert_blank_line_after_output = False
 
@@ -86,52 +68,39 @@ def configure(repl):
     #       otherwise, when there is a completion available, the arrows will
     #       browse through the available completions instead of the history.
     repl.enable_history_search = False
-
     # Enable auto suggestions. (Pressing right arrow will complete the input,
     # based on the history.)
     repl.enable_auto_suggest = True
-
     # Enable open-in-editor. Pressing C-x C-e in emacs mode or 'v' in
     # Vi navigation mode will open the input in the current editor.
     repl.enable_open_in_editor = True
-
     # Enable system prompt. Pressing meta-! will display the system prompt.
     # Also enables Control-Z suspend.
     repl.enable_system_bindings = True
-
     # Ask for confirmation on exit.
     repl.confirm_exit = False
-
-    # Enable input validation. (Don't try to execute when the input contains
-    # syntax errors.)
+    # Don't try to execute when the input contains syntax errors.
     repl.enable_input_validation = False
-
     # Use this colorscheme for the code.
     repl.use_code_colorscheme("monokai")
 
-    # Set color depth (keep in mind that not all terminals support true color).
-
     # repl.color_depth = 'DEPTH_1_BIT'  # Monochrome.
     # repl.color_depth = 'DEPTH_4_BIT'  # ANSI colors only.
-#     repl.color_depth = "DEPTH_8_BIT"  # The default, 256 colors.
+    # repl.color_depth = "DEPTH_8_BIT"  # The default, 256 colors.
     repl.color_depth = 'DEPTH_24_BIT'  # True color.
 
     # Syntax.
     repl.enable_syntax_highlighting = True
 
     # Install custom colorscheme named 'my-colorscheme' and use it.
-    """
-    repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
-    repl.use_ui_colorscheme('my-colorscheme')
-    """
+#     repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
+#     repl.use_ui_colorscheme('my-colorscheme')
 
     # Add custom key binding for PDB.
-    """
     @repl.add_key_binding(Keys.ControlB)
     def _(event):
         ' Pressing Control-B will insert "pdb.set_trace()" '
         event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
-    """
 
     # Typing ControlE twice should also execute the current command.
     # (Alternative for Meta-Enter.)
@@ -143,10 +112,12 @@ def configure(repl):
 
     # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
     # mode.)
-#     @repl.add_key_binding('j', 'j', filter=ViInsertMode())
-#     def _(event):
-#         " Map 'jj' to Escape. "
-#         event.cli.key_processor.feed(KeyPress(Keys.Escape))
+    def escape(event):
+        event.cli.key_processor.feed(KeyPress(Keys.Escape))
+    repl.add_key_binding('j', 'j', filter=ViInsertMode())(escape)
+    repl.add_key_binding('k', 'k', filter=ViInsertMode())(escape)
+    repl.add_key_binding('h', 'h', filter=ViInsertMode())(escape)
+    repl.add_key_binding('l', 'l', filter=ViInsertMode())(escape)
 
     # Custom key binding for some simple autocorrection while typing.
     corrections = {
@@ -164,7 +135,6 @@ def configure(repl):
             if w in corrections:
                 b.delete_before_cursor(count=len(w))
                 b.insert_text(corrections[w])
-
         b.insert_text(' ')
 
 
