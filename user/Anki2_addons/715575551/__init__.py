5daf5e7cdc075ad5237d516c2a96d28559b2cf6b
Carlos M. Cabrera <carlos.cabrera@globality.com>
2020-10-27 18:07:10 +0000

    updated user files

--- user/Anki2_addons/715575551/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) Yutsuten <https://github.com/Yutsuten>. Licensed under AGPL-3.0.
-See the LICENCE file in the repository root for full licence text.
-"""
-
-from . import main
-
-main.main()
